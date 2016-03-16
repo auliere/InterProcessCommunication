ec47686c587f0ec0dbbfbabd821b5b56f3875ba6
IlyaEastSide <olapuh84@gmail.com>
2016-03-16 15:35:38 +0200

    Transferred array

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -11,8 +11,9 @@ socket.connect({
 }, function() {
   socket.write('Hello from client');
   socket.on('data', function(data) {
-    //user = JSON.parse(data);
+    user = JSON.parse(data);
     console.log('Data received (by client): ' + data);
     //console.log('Age of ' + user.name + ' is ' + user.age);
+	console.log(user[3]);
   });
 });
